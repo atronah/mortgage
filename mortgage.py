178ff5e6c5e947d191d677c0a464870875420038
atronah <atronah.ds@gmail.com>
2018-08-18 02:45:27 +0300

    ft(mortgage): rewrites all logic and adds support extra payments

--- mortgage.py
+++ mortgage.py
@@ -1,57 +1,97 @@
-from datetime import date
+from datetime import date, timedelta
 from calendar import monthrange
 
-
 class Mortgage(object):
-    def __init__(self, total, rate, duration, start_date):
-        self.total = total
+    def __init__(self, start_date, amount, rate, duration):
+        self.start_date = start_date
+        self.amount = amount
         self.rate = rate
         self.duration = duration
-        self.start_date = start_date
-        self.data = {}
-        
-    def recalculate(self):
-        on_date = self.start_date
-        info = self.data.setdefault(on_date, {})
-        info['upb'] = self.total # unpaid principal balance
-        info['interest_charges'] = 0.0
-        info['mpr'] = 0.0 # monthly principal repayment
-        info['epr'] = 0.0 # early partial repayment (extra repayments)
-        
-        for n in range(self.duration):
-            prev_date = on_date
-            year = prev_date.year + (0 if prev_date.month < 12 else 1)
-            month = ((prev_date.month + 1) % 12) or 12
-            on_date = date(year,
-                           month,
-                           min(prev_date.day, monthrange(year, month)[1]))
-            info = self.data.setdefault(on_date, {})
-            prev_info = self.data[prev_date]
-            
-            
-            if prev_info['mpr'] and not prev_info.get('epr', None):
-                info['mpr'] = prev_info['mpr']
-            else:
-                month_rate = self.rate / 100 / 12 # 12 - months in year, 100 - % 
-                repayments_left = self.duration - n
-                info['mpr'] = round(prev_info['upb'] * month_rate / (1 - (1 + month_rate)**(-repayments_left)), 2)
-                
-            days_in_year = (date(on_date.year + 1, 1, 1) - date(on_date.year, 1, 1)).days
-            days_in_prev_year = days_in_year if on_date.year == prev_date.year \
-                                else (date(prev_date.year + 1, 1, 1) - date(prev_date.year, 1, 1)).days
-            if days_in_year == days_in_prev_year:
-                days_rate = self.rate * (on_date - prev_date).days / (days_in_year * 100)
-            else:
-                days_rate = (date(prev_date.year, 12, 31) - prev_date).days / days_in_prev_year
-                days_rate += ((on_date - date(on_date.year, 1, 1)).days + 1) / days_in_year
-                days_rate = self.rate * days_rate / 100
-                
-            info['interest_charges'] = round(prev_info['upb'] * days_rate, 2)
-            info['upb'] = round(prev_info['upb'] + info['interest_charges'] - info['mpr'], 2)
-            if info['upb'] <= 300:
-                info['mpr'] = round(info['mpr'] + info['upb'], 2)
-                info['upb'] = 0
-    
-    def add_prepayment(self, prepayment_date, amount, prepayment_type):
-        raise NotImplementedError
-    
+        self.holidays = []
+        self.extra_payments = []
+
+    @staticmethod
+    def add_months(to_date, months_count=1):
+        month = to_date.month + months_count
+        year = to_date.year + (month - 1) // 12
+        month = month % 12 or 12
+        day = min(to_date.day, monthrange(year, month)[1])
+        return date(year, month, day)
+
+    @staticmethod
+    def calculate_montly_payment(balance, rate, repayments_left):
+        month_rate = rate / 100 / 12 # 12 - months in year, 100 - %
+        return balance * month_rate / (1 - (1 + month_rate)**(-repayments_left))
+
+    @staticmethod
+    def calculate_interest_charges(from_date, to_date, balance, rate):
+        days_in_year = (date(to_date.year + 1, 1, 1) - date(to_date.year, 1, 1)).days
+        days_in_prev_year = days_in_year \
+                            if to_date.year == from_date.year \
+                            else (date(from_date.year + 1, 1, 1) - date(from_date.year, 1, 1)).days
+        if days_in_year == days_in_prev_year:
+            days_rate = rate * (to_date - from_date).days / (days_in_year * 100)
+        else:
+            days_rate = (date(from_date.year, 12, 31) - from_date).days / days_in_prev_year
+            days_rate += ((to_date - date(to_date.year, 1, 1)).days + 1) / days_in_year
+            days_rate = rate * days_rate / 100
+        return balance * days_rate
+
+
+    def is_holiday(self, checked_date):
+        return checked_date.weekday() in (5, 6) or checked_date in self.holidays
+
+    def skip_holidays(self, for_date):
+        while self.is_holiday(for_date):
+            for_date += timedelta(days=1)
+        return for_date
+
+    def add_extra_payment(self, payment_date, payment_amount):
+        self.extra_payments.append((payment_date, payment_amount))
+
+    def repayments_schedule(self):
+        extra_payments = self.extra_payments
+        extra_payments.sort(key=lambda x: x[0])
+        extra_payment = None
+
+        adate = self.start_date
+        fdate = self.skip_holidays(adate)
+        payment = 0.0
+        interest_charge = 0.0
+        balance = self.amount
+        is_extra = False
+
+        yield adate, fdate, payment, interest_charge, balance, is_extra
+
+        payment_number = 1
+        extra_payment = extra_payments.pop(0) if extra_payments else None
+        recalculate_payment = True
+        while payment_number <= self.duration:
+            prev_date = adate
+            adate = self.add_months(self.start_date, payment_number)
+
+            interest_charge = round(self.calculate_interest_charges(prev_date, adate, balance, self.rate), 2)
+            if extra_payment and extra_payment[0] < adate:
+                payment_number -= 1
+                is_extra = True
+                adate, payment = extra_payment
+                extra_payment = extra_payments.pop(0) if extra_payments else None
+                interest_charge = round(self.calculate_interest_charges(prev_date, adate, balance, self.rate), 2)
+            elif is_extra:
+                payment = interest_charge
+                is_extra = False
+                recalculate_payment = True
+            elif recalculate_payment:
+                payment = self.calculate_montly_payment(balance, self.rate, self.duration - payment_number + 1)
+                recalculate_payment = False
+
+            fdate = self.skip_holidays(adate)
+            balance = round(balance + interest_charge - payment, 2)
+            if balance < 300:
+                payment = payment + balance
+                balance = 0.0
+            payment = round(payment, 2)
+
+            yield adate, fdate, payment, interest_charge, balance, is_extra
+
+            payment_number += 1
